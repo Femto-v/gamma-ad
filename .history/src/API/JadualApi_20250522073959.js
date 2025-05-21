2c43c143718147918300bdbc790e935e556e9d83
Femto <90186633+Femto-v@users.noreply.github.com>
2025-05-22 07:51:57 +0800

    revise jadualAPI

--- .history/src/API/JadualApi_20250522073959.js
+++ .history/src/API/JadualApi_20250522073959.js
@@ -1,12 +1,9 @@
 import BaseApi from "./BaseApi";
 
 export default class JadualApi extends BaseApi {
-    async getJadualInfo(no_matrik, sesi, semester) {
+    async getJadualInfo() {
         return this.get({
             entity: "pelajar_subjek",
-            no_matrik: no_matrik,
-            sesi: sesi,
-            semester: semester,
         });
     }
 }
